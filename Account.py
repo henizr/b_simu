749e2fb4acb856d70bf61427bd2e4621eeee9902
Harry <henry.iz@yandex.ru>
2024-03-02 14:09:29 +0300

    Added the withdraw and show methods

--- Account.py
+++ Account.py
@@ -16,3 +16,30 @@ class Account:
         self.balance = self.balance + amountToDeposit
         
         return self.balance
+    
+    def withdraw(self, amountToWithdraw, password):
+        if password != self.password:
+            print("Sorry, incorrect password")
+            return None
+        if amountToWithdraw < 0:
+            print("You cannot withdraw a negative amount")    
+            return None
+        if amountToWithdraw > self.balance:
+            print("You cannot withdraw more than you have in your account")
+            return None
+        
+        self.balance = self.balance - amountToWithdraw
+        return self.balance
+    
+    def getBalance(self, password):
+        if password != self.password:
+            print("Sorry, incorrect password")
+            return None
+        
+        return self.balance
+    
+    def show(self):
+        print("Name: ", self.name)
+        print("Balance: ", self.balance)
+        print()
+        
